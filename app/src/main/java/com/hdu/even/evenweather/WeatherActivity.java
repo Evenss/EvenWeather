f58e29552fa0097537ccbc7ac6a3b71bd60764cd
even <786007448@qq.com>
2017-03-12 23:10:56 +0800

    UI bug fixed

--- app/src/main/java/com/hdu/even/evenweather/WeatherActivity.java
+++ app/src/main/java/com/hdu/even/evenweather/WeatherActivity.java
@@ -12,6 +12,8 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -71,12 +73,20 @@ public class WeatherActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * 修改标题栏和导航栏为透明
+     */
     private void alterTitleBar(){
         if(Build.VERSION.SDK_INT >=21){
-            View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility
-                    (View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
+                    | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+            window.setNavigationBarColor(Color.TRANSPARENT);
         }
     }
 
